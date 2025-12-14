baf43bc4da840d8d820067e70cf9d35de5fc5d7d
beingborn <dolmin0708@naver.com>
2025-12-14 11:07:14 +0900

    Global Layout Markup, Global Font Setting

--- src/App.vue
+++ src/App.vue
@@ -1,86 +1,42 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import { RouterView } from 'vue-router';
 </script>
 
+<!-- 
+  1. 폰트 세팅
+  2. 헤더 푸터 작업 진행 
+  3. 라우터 세팅 
+  4. GuestOnly, MemberOnly 
+-->
+
 <template>
-  <div class="flex items-center justify-center min-h-screen bg-gradient-to-r from-blue-500 to-purple-600">
-    <div class="bg-white p-8 rounded-lg shadow-2xl text-center">
-      <h1 class="text-4xl font-bold text-gray-800 mb-4">
-        Tailwind CSS Works! 🎉
-      </h1>
-      <p class="text-gray-600 text-lg">
-        Vue 3 + Tailwind CSS
-      </p>
-      <button class="mt-6 px-6 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600 transition">
-        Click Me
-      </button>
-    </div>
+  <div class="flex flex-col items-center min-h-screen">
+    <header>
+      <div class="w-full max-w-[1300px] flex items-center justify-between h-10 border">
+        <h1>
+          비보
+        </h1>
+        <div class="flex items-center gap-2">
+          <button type="button">
+          테마
+          </button>
+          <button type="button">
+            프로필
+          </button>
+        </div>
+      </div>
+    </header>
+
+    <main class="grow max-w-[1300px] mx-auto p-8 border w-full">
+      <RouterView />
+    </main>
+
+    <footer>
+      <div class="flex items-center">
+        <p>@2025 mhlee reserved</p>
+      </div>
+    </footer>
   </div>
+  
 
-  <RouterView />
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
-</style>
--- src/main.js
+++ src/main.js
@@ -1,9 +1,8 @@
-import './assets/main.css'
-
-import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { createApp } from 'vue'
 
 import App from './App.vue'
+import './assets/base.css'
 import router from './router'
 import './style.css'
 
